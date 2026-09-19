3342c5716990948e1383227c9eea07ecebb9fbe2
AndreLucax12 <190207785+AndreLucax12@users.noreply.github.com>
2026-09-19 17:34:25 -0300

    feat: cabecalho e rodape em componentes separados

---
+++ src/componentes/Cabecalho.tsx
@@ -0,0 +1,8 @@
+export function Cabecalho() {
+  return (
+    <header>
+      <h1>Controlador de Estoque</h1>
+      <p>Gerencie seus produtos e estoque de forma eficiente</p>
+    </header>
+  );
+}
---
+++ src/componentes/Rodape.tsx
@@ -0,0 +1,7 @@
+export function Rodape() {
+  return(
+    <footer>
+      <p>&copy; 2026 TechOS. Todos os direitos reservados.</p>
+    </footer>
+  );
+}
