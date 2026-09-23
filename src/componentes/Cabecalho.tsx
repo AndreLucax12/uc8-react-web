d78b66456c0345d3c244a946b1d61437f89c9ba1
AndreLucax12 <190207785+AndreLucax12@users.noreply.github.com>
2026-09-23 19:12:14 -0300

    chore: cabecalho com a identidade TechOS

--- src/componentes/Cabecalho.tsx
+++ src/componentes/Cabecalho.tsx
@@ -1,8 +1,8 @@
 export function Cabecalho() {
   return (
     <header>
-      <h1>Controlador de Estoque</h1>
-      <p>Gerencie seus produtos e estoque de forma eficiente</p>
+      <h1>TechOS</h1>
+      <p>Gerenciador de assistência técnica</p>
     </header>
   );
 }
